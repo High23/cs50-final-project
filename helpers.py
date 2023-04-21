3e624525654c283af9331c723be82254d9092bb4
High23 <warenasb2@gmail.com>
2023-04-20 21:47:29 -0700

    not needed

--- helpers.py
+++
@@ -1,7 +0,0 @@
-import webbrowser 
-
-urL='https://www.google.com'
-chrome_path="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
-webbrowser.get('chrome').open_new_tab(urL)
-print("end of code")
